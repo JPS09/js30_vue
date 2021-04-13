d8268eed3613721d4702df8ad33e82a88e2e32ee
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-04-13 10:17:31 +0200

    First day data

--- src/store/modules/days/index.js
+++ src/store/modules/days/index.js
@@ -5,7 +5,16 @@ import daysActions from "./actions.js";
 export default {
   state() {
     return {
-      days: [{}],
+      days: [
+        {
+          id: "d1",
+          name: " Day 1",
+          description:
+            "This challenge makes use of an array of basics DOM manipulations techniques such as event listening and conditonal CSS classes",
+          categories: ["tips", "drums"],
+          viewed: false,
+        },
+      ],
     };
   },
   mutations: daysMutations,
