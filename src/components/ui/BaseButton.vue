20ddeb452791dbdb07734433059c56f292853ad0
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-04-15 15:13:58 +0200

    Hover styling on button

--- src/components/ui/BaseButton.vue
+++ src/components/ui/BaseButton.vue
@@ -11,11 +11,19 @@ button {
   border: none;
   background: white;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  color: #707070;
   border-radius: 30px;
   width: 20vw;
   height: 4vh;
   min-width: 100px;
   cursor: pointer;
   margin: 1rem;
+  transition: all 0.2s ease-in-out;
+  font-weight: 800;
+}
+
+button:hover {
+  background: #458b51;
+  color: white;
 }
 </style>
