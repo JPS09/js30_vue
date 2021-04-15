c7879121bdcb0c06751aec7bda1ae5b09a30818b
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-04-15 10:43:46 +0200

    Fixed alignment on nav bar items

--- src/components/ui/Nav.vue
+++ src/components/ui/Nav.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="nav-bar">
-    <img
-      src="../../assets/house-black-silhouette-without-door.svg"
-      alt="Link to Home page"
-      class="home-logo"
-      @click="homeLink"
-    />
+    <div id="left">
+      <img
+        src="../../assets/house-black-silhouette-without-door.svg"
+        alt="Link to Home page"
+        class="home-logo"
+        @click="homeLink"
+      />
+    </div>
 
-    <p>JS30 with <span class="vue-highlight">Vue</span></p>
-    <div id="placeholder-div"></div>
+    <h1 id="center">JS30 with <span class="vue-highlight">Vue</span></h1>
   </div>
 </template>
 
@@ -23,19 +24,29 @@ export default {
 </script>
 
 <style scoped>
-#placeholder-div {
-  width: 10px;
+#center {
+  flex: 1;
+}
+#left {
+  flex: 1;
+  display: flex;
+  align-items: flex-start;
 }
 .nav-bar {
   position: sticky;
   top: 0;
   display: flex;
   align-items: center;
-  justify-content: space-between;
+  justify-content: space-around;
   background: white;
   box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.26);
   min-height: 10vh;
   width: 100vw;
+}
+
+.nav-bar::after {
+  content: "";
+  flex: 1;
 }
 .home-logo {
   height: 8vh;
