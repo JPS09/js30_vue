053a06fc1679ba69bfd1ff2536fde797028427a2
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-04-29 16:29:37 +0200

    Started placing search icon into search bar

--- src/components/search/SearchInput.vue
+++ src/components/search/SearchInput.vue
@@ -1,6 +1,8 @@
 <template>
-  <input class="input" type="text" placeholder="Search for a day" />
-  <font-awesome-icon icon="search" />
+  <div class="search-container">
+    <input class="input" type="text" placeholder="Search for a day" />
+    <font-awesome-icon icon="search" class="searchIcon" />
+  </div>
 </template>
 
 <script>
@@ -8,6 +10,17 @@ export default {};
 </script>
 
 <style scoped>
+.search-container {
+  display: grid;
+  grid-template-columns: 4fr 1fr;
+  width: 20em;
+}
+
+.searchIcon {
+  grid-row: 1;
+  grid-column: 2;
+  z-index: 2;
+}
 .input {
   border-radius: 30px;
   border: none;
