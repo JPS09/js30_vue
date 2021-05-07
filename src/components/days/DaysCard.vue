3ab19198b710de448d2e9bc01660057c9cb59222
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-05-07 10:19:21 +0200

    Created Day card WIP

---
+++ src/components/days/DaysCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <base-card>
+    <h3>{{ DayNumber }}</h3>
+    <h4>{{ DayName }}</h4>
+    <p>{{ DayDescription }}</p>
+  </base-card>
+</template>
+
+<script>
+export default {
+  props: {
+    DayNumber: {
+      type: String,
+      required: true,
+    },
+    DayName: {
+      type: String,
+      required: true,
+    },
+    DayDescription: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
