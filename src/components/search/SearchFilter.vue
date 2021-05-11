1a073a30b5acedb9cf268d06960b5e532eda333b
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-05-11 09:54:09 +0200

    Created select component to filter days

---
+++ src/components/search/SearchFilter.vue
@@ -0,0 +1,9 @@
+<template>
+  <select name="days" id="day-select">
+    <option value="default">Filter the days</option>
+    <option value="essentials">Essentials</option>
+    <option value="tips">Tips</option>
+    <option value="fun">Fun</option>
+    <option value="nav">Nav</option>
+  </select>
+</template>
