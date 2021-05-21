7e324a76209648cc26c9d5918d0cfae5279c9397
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-05-21 09:51:18 +0200

    Started styling voice dialog box

--- src/components/search/SearchDialog.vue
+++ src/components/search/SearchDialog.vue
@@ -2,10 +2,12 @@
   <base-dialog :dialogDisplay="dialogDisplay" :voiceDialog="true">
     <font-awesome-icon icon="time"></font-awesome-icon>
     <template #voiceMic>
-      <div class="search-dialog__outer-circle">
-        <div class="search-dialog__middle-circle">
-          <div class="search-dialog__inner-circle">
-            <font-awesome-icon icon="microphone"></font-awesome-icon>
+      <div class="search-dialog__container">
+        <div class="search-dialog__outer-circle">
+          <div class="search-dialog__middle-circle">
+            <div class="search-dialog__inner-circle">
+              <font-awesome-icon icon="microphone"></font-awesome-icon>
+            </div>
           </div>
         </div>
       </div>
@@ -30,7 +32,8 @@ export default {
 </script>
 
 <style scoped>
-.search-dialog__container {
+.search-dialog__container,
+div > div {
   display: flex;
   align-items: center;
   justify-content: center;
@@ -39,5 +42,25 @@ export default {
 .search-dialog__middle-circle,
 .search-dialog__inner-circle {
   border-radius: 50%;
+}
+
+.search-dialog__outer-circle {
+  background: rgba(255, 99, 71, 0.418);
+  width: 10em;
+  height: 10em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.search-dialog__inner-circle {
+  width: 5em;
+  height: 5em;
+  background: turquoise;
+}
+.search-dialog__middle-circle {
+  width: 8em;
+  height: 8em;
+  background: rgba(210, 105, 30, 0.678);
 }
 </style>
