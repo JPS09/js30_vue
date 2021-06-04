a06500e0fc2435eed13662cca977777d2a4f0fbe
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-06-04 16:54:37 +0200

    Removed useless div

--- src/components/stats/ViewingCategoryCompletion.vue
+++ src/components/stats/ViewingCategoryCompletion.vue
@@ -1,12 +1,10 @@
 <template>
   <stats-styling-wrapper>
     <h3 id="viewing-category-completion__viewing-title">Viewing Stats</h3>
-    <div>
-      <viewing-category-item categoryName="Basics"></viewing-category-item>
-      <viewing-category-item categoryName="Tips"></viewing-category-item>
-      <viewing-category-item categoryName="Fun"></viewing-category-item>
-      <viewing-category-item categoryName="Nav"></viewing-category-item>
-    </div>
+    <viewing-category-item categoryName="Basics"></viewing-category-item>
+    <viewing-category-item categoryName="Tips"></viewing-category-item>
+    <viewing-category-item categoryName="Fun"></viewing-category-item>
+    <viewing-category-item categoryName="Nav"></viewing-category-item>
   </stats-styling-wrapper>
 </template>
 
