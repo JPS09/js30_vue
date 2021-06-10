749b1c205beb9361d93f5874c975ad6a88b73885
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-06-10 10:30:19 +0200

    Added several classes on day differences component

--- src/components/days/DayDifferences.vue
+++ src/components/days/DayDifferences.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="day-differences">
+  <div class="day-differences__container">
     <header id="day-differences__header">
       <h1>Main differences</h1>
     </header>
-    <section>
+    <section class="day-differences__section">
       <h3 class="day-differences__title">JS</h3>
-      <p>Blabla le js</p>
+      <p class="day-differences__details">Blabla le js</p>
     </section>
-    <section>
+    <section class="day-differences__section">
       <h3 class="day-differences__title">Vue</h3>
-      <p>Blabla Vue</p>
+      <p class="day-differences__details">Blabla Vue</p>
     </section>
   </div>
 </template>
@@ -27,13 +27,22 @@
   padding: 2rem;
 }
 
-.day-differences {
+.day-differences__container {
   border-radius: 12px;
   background: white;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding-bottom: 1em;
 }
 .day-differences__title {
   text-align: left;
+}
+
+.day-differences__section {
   padding: 0 2rem;
+}
+.day-differences__details {
+  margin: 0;
+  padding: 0 2rem;
+  text-align: justify;
 }
 </style>
