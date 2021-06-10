66f2c1ea0cdfc650437899a69ddb6c22695493d5
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-06-10 09:41:43 +0200

    Basic day card details styling

--- src/components/days/DayDetails.vue
+++ src/components/days/DayDetails.vue
@@ -1,9 +1,11 @@
 <template>
-  <div>
-    <h3>{{ dayName }}</h3>
-    <p>{{ dayDescription }}</p>
-    <p>{{ dayCategory }}</p>
-  </div>
+  <base-card>
+    <div class="day-details__infos">
+      <h2 id="day-details__name">{{ dayName }}</h2>
+      <h4 class="day-details__category">{{ dayCategory }}</h4>
+    </div>
+    <p class="day-details__description">{{ dayDescription }}</p>
+  </base-card>
 </template>
 
 <script>
@@ -24,3 +26,18 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.day-details__infos {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 1em;
+}
+
+.day-details__description {
+  padding: 1em;
+  text-align: justify;
+  margin: 0;
+}
+</style>
