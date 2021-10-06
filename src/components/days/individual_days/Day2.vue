45070e0ae68cac77be78eb205746dc261e58de9a
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-10-06 15:39:10 +0200

    conditional fix on query selector

--- src/components/days/individual_days/Day2.vue
+++ src/components/days/individual_days/Day2.vue
@@ -10,23 +10,29 @@
 <script>
 const setSeconds = (now) => {
   const secondHand = document.querySelector(".second-hand");
-  const seconds = now.getSeconds();
-  const secondsDegrees = (seconds / 60) * 360 + 90;
-  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  if (secondHand) {
+    const seconds = now.getSeconds();
+    const secondsDegrees = (seconds / 60) * 360 + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  }
 };
 
 const setMinutes = (now) => {
   const minHand = document.querySelector(".min-hand");
-  const minutes = now.getMinutes();
-  const minutesDegrees = (minutes / 60) * 360 + 90;
-  minHand.style.transform = `rotate(${minutesDegrees}deg)`;
+  if (minHand) {
+    const minutes = now.getMinutes();
+    const minutesDegrees = (minutes / 60) * 360 + 90;
+    minHand.style.transform = `rotate(${minutesDegrees}deg)`;
+  }
 };
 
 const setHour = (now) => {
   const hourHand = document.querySelector(".hour-hand");
-  const hour = now.getHours();
-  const hourDegrees = (hour / 12) * 360 + 90;
-  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+  if (hourHand) {
+    const hour = now.getHours();
+    const hourDegrees = (hour / 12) * 360 + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+  }
 };
 const setDate = () => {
   const now = new Date();
