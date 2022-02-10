62b8b46fa04f935135c7355ef6edc3e31cbac6c7
Jean-Pierre Symoens <jpsymoens0@gmail.com>
2022-02-10 12:53:30 +0100

    Adding getters for stats WIP

--- src/store/modules/stats/getters.js
+++ src/store/modules/stats/getters.js
@@ -1,1 +1,17 @@
-export default {};
+export default {
+  totalStats(_, _2, _3, rootGetters) {
+    rootGetters.days;
+  },
+  basicsStats(_, _2, _3, rootGetters) {
+    rootGetters.basicsDays;
+  },
+  funStats(_, _2, _3, rootGetters) {
+    rootGetters.funDays;
+  },
+  tipsStats(_, _2, _3, rootGetters) {
+    rootGetters.tipsDays;
+  },
+  navStats(_, _2, _3, rootGetters) {
+    rootGetters.navDays;
+  },
+};
