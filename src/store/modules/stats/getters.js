58183940a13534236510b7f3e179e3876a1cbeb2
Jean-Pierre Symoens <jpsymoens0@gmail.com>
2022-02-10 13:24:51 +0100

    Added logic to count stats

--- src/store/modules/stats/getters.js
+++ src/store/modules/stats/getters.js
@@ -1,17 +1,43 @@
 export default {
   totalStats(_, _2, _3, rootGetters) {
-    rootGetters.days;
+    let count = 0;
+    rootGetters.days.forEach((day) => (day.viewed ? (count = +1) : count));
+    return count;
   },
   basicsStats(_, _2, _3, rootGetters) {
-    rootGetters.basicsDays;
+    let basicCount = 0;
+    rootGetters.days.forEach((day) => {
+      if (day.category === "Basics") {
+        day.viewed ? (basicCount = +1) : basicCount;
+      }
+    });
+    return basicCount;
   },
   funStats(_, _2, _3, rootGetters) {
-    rootGetters.funDays;
+    let funCount = 0;
+    rootGetters.days.forEach((day) => {
+      if (day.category === "Fun") {
+        day.viewed ? (funCount = +1) : funCount;
+      }
+    });
+    return funCount;
   },
   tipsStats(_, _2, _3, rootGetters) {
-    rootGetters.tipsDays;
+    let tipsCount = 0;
+    rootGetters.days.forEach((day) => {
+      if (day.category === "Tips") {
+        day.viewed ? (tipsCount = +1) : tipsCount;
+      }
+    });
+    return tipsCount;
   },
   navStats(_, _2, _3, rootGetters) {
-    rootGetters.navDays;
+    let navCount = 0;
+    rootGetters.days.forEach((day) => {
+      if (day.category === "Nav") {
+        day.viewed ? (navCount = +1) : navCount;
+      }
+    });
+    return navCount;
   },
 };
