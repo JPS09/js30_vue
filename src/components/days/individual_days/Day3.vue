c5729b3dae514e3c7f0e306794b69be8b0ecce96
Jean-Pierre Symoens <jpsymoens0@gmail.com>
2022-03-17 19:18:19 +0100

    Working color

--- src/components/days/individual_days/Day3.vue
+++ src/components/days/individual_days/Day3.vue
@@ -71,7 +71,7 @@ export default {
       return `--spacing: ${this.spacing}px`;
     },
     colorValue() {
-      return `--color: ${this.color}`;
+      return `--base: ${this.color}`;
     },
   },
 };
