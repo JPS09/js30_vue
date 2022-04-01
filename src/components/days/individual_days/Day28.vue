9c9966705be7e9eb6f0e9329f6e0627a1b61d6be
Jean-Pierre Symoens <jpsymoens0@gmail.com>
2022-04-01 13:23:26 +0200

    Working Day28

--- src/components/days/individual_days/Day28.vue
+++ src/components/days/individual_days/Day28.vue
@@ -2,39 +2,39 @@
   <div class="wrapper">
     <video
       class="flex"
+      ref="video"
       width="765"
       height="430"
       src="http://clips.vorwaerts-gmbh.de/VfE_html5.mp4"
       loop
       controls
     ></video>
-    <div class="speed">
-      <div class="speed-bar">1×</div>
+    <div class="speed" ref="speed" @mousemove="hoverMouse($event)">
+      <div class="speed-bar" ref="speedBar">1x</div>
     </div>
   </div>
 </template>
 
 <script>
-const speed = document.querySelector(".speed");
-const bar = speed.querySelector(".speed-bar");
-const video = document.querySelector(".flex");
-
-function hoverMouse(e) {
-  const y = e.pageY - this.offsetTop;
-  const percent = y / this.offsetHeight;
-  const min = 0.4;
-  const max = 4;
-  // Rounds the height value to make it usable in CSS
-  const height = Math.round(percent * 100) + "%";
-  bar.style.height = height;
-  // Calculate the playback rate depending on the fill percentage of the input and the min/ max values
-  const playbackRate = percent * (max - min) + min;
-  // Show the playbackRate in the front
-  bar.textContent = `${playbackRate.toFixed(2)}x`;
-  // Applies the playbackRate to the video
-  video.playbackRate = playbackRate;
-}
-speed.addEventListener("mousemove", hoverMouse);
+export default {
+  methods: {
+    hoverMouse(e) {
+      const y = e.pageY - this.$refs.speed.offsetTop;
+      const percent = y / this.$refs.speed.offsetHeight;
+      const min = 0.4;
+      const max = 4;
+      // Rounds the height value to make it usable in CSS
+      const height = Math.round(percent * 100) + "%";
+      this.$refs.speedBar.style.height = height;
+      // Calculate the playback rate depending on the fill percentage of the input and the min/ max values
+      const playbackRate = percent * (max - min) + min;
+      // Show the playbackRate in the front
+      this.$refs.speedBar.textContent = `${playbackRate.toFixed(2)}x`;
+      // Applies the playbackRate to the video
+      this.$refs.video.playbackRate = playbackRate;
+    },
+  },
+};
 </script>
 
 <style scoped>
