3ca6811c5dbdceadd3d7e2318dcdda85899b8c4e
Jean-Pierre Symoens <jpsymoens0@gmail.com>
2022-04-07 09:57:26 +0200

    Working sound AND transition

--- src/components/days/individual_days/Day1.vue
+++ src/components/days/individual_days/Day1.vue
@@ -5,15 +5,68 @@
       :key="audioKey.dataKey"
       :data-key="audioKey.dataKey"
       class="key"
-      :class="[audioKey.isActive ? 'playing' : '', 'key']"
-      @click="playSound($event)"
-      @transitionend="removeTransition($event, audioKey)"
+      @transitionend="removeTransition($event)"
     >
-      <!-- @keyup-name-of-the-key="tryingSound" -->
       <kbd>{{ audioKey.kbd }}</kbd>
       <span class="sound">{{ audioKey.name }}</span>
     </div>
   </div>
+
+  <audio data-key="65">
+    <source
+      type="audio/wav"
+      src="../../../assets/days_assets/Day1/sounds/clap.wav"
+    />
+    >
+  </audio>
+  <audio data-key="83">
+    <source
+      type="audio/wav"
+      src="../../../assets/days_assets/Day1/sounds/hihat.wav"
+    />
+  </audio>
+  <audio data-key="68">
+    <source
+      type="audio/wav"
+      src="../../../assets/days_assets/Day1/sounds/kick.wav"
+    />
+  </audio>
+  <audio data-key="70">
+    <source
+      type="audio/wav"
+      src="../../../assets/days_assets/Day1/sounds/openhat.wav"
+    />
+  </audio>
+  <audio data-key="71">
+    <source
+      src="../../../assets/days_assets/Day1/sounds/boom.wav"
+      type="audio/wav"
+    />
+  </audio>
+  <audio data-key="72">
+    <source
+      src="../../../assets/days_assets/Day1/sounds/ride.wav"
+      type="audio/wav"
+    />
+  </audio>
+  <audio data-key="74">
+    <source
+      src="../../../assets/days_assets/Day1/sounds/snare.wav"
+      type="audio/wav"
+    />
+  </audio>
+  <audio data-key="75">
+    <source
+      src="../../../assets/days_assets/Day1/sounds/tom.wav"
+      type="audio/wav"
+    />
+  </audio>
+  <audio data-key="76">
+    <source
+      src="../../../assets/days_assets/Day1/sounds/tink.wav"
+      type="audio/wav"
+    />
+  </audio>
 </template>
 
 <script>
@@ -22,127 +75,104 @@ export default {
     return {
       audioKeys: [
         {
-          audio: new Audio("../../../assets/days_assets/Day1/sounds/clap.wav"),
+          type: "audio/wav",
+          src: "../../../assets/days_assets/Day1/sounds/clap.wav",
           name: "clap",
-          dataKey: 65,
+          dataKey: "65",
           kbd: "A",
-          isActive: false,
         },
         {
           type: "audio/wav",
-          audio: new Audio("../../../assets/days_assets/Day1/sounds/hihat.wav"),
+          src: "../../../assets/days_assets/Day1/sounds/hihat.wav",
           name: "hihat",
-          dataKey: 83,
+          dataKey: "83",
           kbd: "S",
-          isActive: false,
         },
         {
           type: "audio/wav",
-          audio: new Audio("../../../assets/days_assets/Day1/sounds/kick.wav"),
+          src: "../../../assets/days_assets/Day1/sounds/kick.wav",
           name: "kick",
-          dataKey: 68,
+          dataKey: "68",
           kbd: "D",
-          isActive: false,
         },
         {
           type: "audio/wav",
-          audio: new Audio(
-            "../../../assets/days_assets/Day1/sounds/openhat.wav"
-          ),
+          src: "../../../assets/days_assets/Day1/sounds/openhat.wav",
           name: "openhat",
-          dataKey: 70,
+          dataKey: "70",
           kbd: "F",
         },
         {
           type: "audio/wav",
-          audio: new Audio("../../../assets/days_assets/Day1/sounds/boom.wav"),
+          src: "../../../assets/days_assets/Day1/sounds/boom.wav",
           name: "boom",
-          dataKey: 71,
+          dataKey: "71",
           kbd: "G",
-          isActive: false,
         },
         {
           type: "audio/wav",
-          audio: new Audio("../../../assets/days_assets/Day1/sounds/ride.wav"),
+          src: "../../../assets/days_assets/Day1/sounds/ride.wav",
           name: "ride",
-          dataKey: 72,
+          dataKey: "72",
           kbd: "H",
-          isActive: false,
         },
         {
           type: "audio/wav",
-          audio: new Audio("../../../assets/days_assets/Day1/sounds/snare.wav"),
+          src: "../../../assets/days_assets/Day1/sounds/snare.wav",
           name: "snare",
-          dataKey: 74,
+          dataKey: "74",
           kbd: "J",
-          isActive: false,
         },
         {
           type: "audio/wav",
-          audio: new Audio("../../../assets/days_assets/Day1/sounds/tom.wav"),
+          src: "../../../assets/days_assets/Day1/sounds/tom.wav",
           name: "tom",
-          dataKey: 75,
+          dataKey: "75",
           kbd: "K",
-          isActive: false,
         },
         {
           type: "audio/wav",
-          audio: new Audio("../../../assets/days_assets/Day1/sounds/tink.wav"),
+          src: "../../../assets/days_assets/Day1/sounds/tink.wav",
           name: "tink",
-          dataKey: 76,
+          dataKey: "76",
           kbd: "L",
-          isActive: false,
         },
       ],
     };
   },
-
   methods: {
-    removeTransition(e, key) {
-      if (e.propertyName !== "transform") {
-        return;
-      }
-      key.isActive = false;
-    },
-    playSound(e) {
-      const key = this.audioKeys.find((key) => {
-        key.audio.type = "audio/wav";
-        return key.dataKey === e.keyCode;
-      });
-      if (!key) {
-        return;
-      }
-      console.log(key.audio);
-      key.audio.currentTime = 0;
-      key.isActive = true;
-      key.audio.play();
+    removeTransition(e) {
+      console.log(e)
+      if (e.propertyName !== "transform") return; // skip if not transform
+      e.target.classList.remove("playing");
     },
   },
-  mounted() {
-    window.addEventListener("keydown", this.playSound);
-  },
-
-  // Maybe watch is a better approach than methods
-  // if (propertyName !== "transform") return; // skip if not transform
-  // target.classList.remove("playing");
+  // methods: {
+  //   tryingSound() {
+  //     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  //     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  //     if (!audio) return; // Stop function if null
+  //     audio.currentTime = 0; // Rewind to start
+  //     key.classList.add("playing");
+  //     audio.play();
+  //   },
+  // },
 };
-// const playSound = (e) => {
-//   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-//   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-//   if (!audio) return; // Stop function if null
-//   audio.currentTime = 0; // Rewind to start
-//   key.classList.add("playing");
-//   audio.play();
-// };
-
-// const removeTransition = (e) => {
-//   if (e.propertyName !== "transform") return; // skip if not transform
-//   e.target.classList.remove("playing");
-// };
-
-// const keys = document.querySelectorAll(".key");
-// keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
-// window.addEventListener("keydown", playSound);
+const playSound = (e) => {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio) return; // Stop function if null
+  audio.currentTime = 0; // Rewind to start
+  key.classList.add("playing");
+  audio.play();
+};
+const removeTransition = (e) => {
+  if (e.propertyName !== "transform") return; // skip if not transform
+  e.target.classList.remove("playing");
+};
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+window.addEventListener("keydown", playSound);
 </script>
 
 <style scoped>
